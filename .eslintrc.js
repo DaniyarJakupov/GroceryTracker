75bc8f93bc8bab6560990dfdac68b8239f468c6b
DaniyarJakupov <ddr199305@gmail.com>
2017-11-27 21:45:21 +0100

    New rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
     'no-underscore-dangle': 0,
     'react/jsx-filename-extension': 0,
     'import/prefer-default-export': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
     'func-names': 0,
   },
 };
