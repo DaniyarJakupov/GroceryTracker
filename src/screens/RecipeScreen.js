bc038aaa411ae6d2d747d0f538edc6c01efe616a
DaniyarJakupov <ddr199305@gmail.com>
2017-11-27 22:00:36 +0100

    Add card to RecipeScreen

--- src/screens/RecipeScreen.js
+++ src/screens/RecipeScreen.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Button } from 'react-native-elements';
+import { ScrollView, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+} from 'native-base';
 
 class RecipeScreen extends Component {
   state = {
@@ -10,16 +22,35 @@ class RecipeScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          large
-          title="Recipes"
-          raised
-          borderRadius={15}
-          containerViewStyle={{ borderRadius: 15 }}
-          backgroundColor="#3A3897"
-        />
-      </View>
+      <Container>
+        <Header />
+        <Content>
+          <Card style={{ flex: 0 }}>
+            <CardItem>
+              <Left>
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>April 15, 2016</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Image source={{ uri: 'Image URL' }} style={{ height: 200, width: 200, flex: 1 }} />
+                <Text>//Your text here</Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={{ color: '#87838B' }}>
+                  <Icon name="logo-github" />
+                  <Text>1,926 stars</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }
