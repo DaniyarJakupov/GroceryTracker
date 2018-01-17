3a90bf9e5e403d30b6461e2bdf0870fdb0fac93b
DaniyarJakupov <ddr199305@gmail.com>
2018-01-17 13:03:23 +0100

    Add list of consumed products

--- src/screens/ConsumptionScreen.js
+++ src/screens/ConsumptionScreen.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
-import { Container, Header, Content, Button, Text, Title, Body } from 'native-base';
+import {
+  Container,
+  Body,
+  Header,
+  Content,
+  Text,
+  Title,
+  List,
+  ListItem,
+  Separator,
+} from 'native-base';
 import { VictoryPie } from 'victory-native';
 import { connect } from 'react-redux';
 
@@ -10,24 +20,74 @@ const Center = styled.View`
 `;
 class ConsumptionScreen extends Component {
   state = {
-    healthyFood: 0,
-    junkFood: 0,
+    healthyFoodCount: 0,
+    junkFoodCount: 0,
+    healthyFood: [],
+    junkFood: [],
   };
 
   componentWillReceiveProps(newProps) {
+    this.setState({ healthyFood: newProps.items.healthyFood, junkFood: newProps.items.junkFood });
     const healthyFoodNumber = newProps.items.healthyFood.length;
     const junkFoodNumber = newProps.items.junkFood.length;
     if (healthyFoodNumber == 0) {
-      this.setState({ junkFood: 100 });
+      this.setState({ junkFoodCount: 100 });
     } else if (junkFoodNumber == 0) {
-      this.setState({ healthyFood: 100 });
+      this.setState({ healthyFoodCount: 100 });
     } else {
       const ratio = healthyFoodNumber / junkFoodNumber;
       const junkFoodPortion = 100 / (ratio + 1);
       const healthyFoodPortion = ratio * junkFoodPortion;
-      this.setState({ healthyFood: healthyFoodPortion, junkFood: junkFoodPortion });
+      this.setState({ healthyFoodCount: healthyFoodPortion, junkFoodCount: junkFoodPortion });
     }
   }
+
+  renderPieChart = () => {
+    if (this.state.healthyFood == 0 && this.state.junkFood == 0) {
+      return (
+        <VictoryPie
+          animate={{ duration: 2000, easing: 'bounce' }}
+          width={370}
+          data={[{ x: 'No data', y: 100 }]}
+          style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
+        />
+      );
+    } else if (this.state.healthyFood == 0) {
+      return (
+        <VictoryPie
+          animate={{ duration: 2000, easing: 'bounce' }}
+          width={370}
+          colorScale={['peru']}
+          data={[{ x: 'Junk food', y: this.state.junkFoodCount }]}
+          style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
+        />
+      );
+    } else if (this.state.junkFood == 0) {
+      return (
+        <VictoryPie
+          animate={{ duration: 2000, easing: 'bounce' }}
+          width={370}
+          colorScale={['#5fb660']}
+          data={[{ x: 'Healthy food', y: this.state.healthyFoodCount }]}
+          style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
+        />
+      );
+    } else {
+      return (
+        <VictoryPie
+          animate={{ duration: 2000, easing: 'bounce' }}
+          colorScale={['#5fb660', 'peru']}
+          width={370}
+          data={[
+            { x: 'Healthy food', y: this.state.healthyFoodCount },
+            { x: 'Junk food', y: this.state.junkFoodCount },
+          ]}
+          labelRadius={60}
+          style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
+        />
+      );
+    }
+  };
 
   render() {
     return (
@@ -38,28 +98,39 @@ class ConsumptionScreen extends Component {
           </Body>
         </Header>
         <Content>
-          <Center>
-            {this.state.healthyFood == 0 && this.state.junkFood == 0 ? (
-              <VictoryPie
-                animate={{ duration: 2000, easing: 'bounce' }}
-                width={370}
-                data={[{ x: 'No data', y: 100 }]}
-                style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
-              />
+          <List>
+            <Center>{this.renderPieChart()}</Center>
+
+            <Separator bordered>
+              <Text style={{ fontSize: 20 }}>Consumed healthy food:</Text>
+            </Separator>
+            {this.state.healthyFood == 0 ? (
+              <ListItem>
+                <Text>No data</Text>
+              </ListItem>
             ) : (
-              <VictoryPie
-                animate={{ duration: 2000, easing: 'bounce' }}
-                colorScale={['#5fb660', 'peru']}
-                width={370}
-                data={[
-                  { x: 'Healthy', y: this.state.healthyFood },
-                  { x: 'Junk', y: this.state.junkFood },
-                ]}
-                labelRadius={60}
-                style={{ labels: { fontSize: 18, fontWeight: 'bold', fill: 'rebeccapurple' } }}
-              />
+              this.state.healthyFood.map(item => (
+                <ListItem key={item.name}>
+                  <Text>{item.name}</Text>
+                </ListItem>
+              ))
             )}
-          </Center>
+
+            <Separator bordered>
+              <Text style={{ fontSize: 20 }}>Consumed junk food: </Text>
+            </Separator>
+            {this.state.junkFood == 0 ? (
+              <ListItem>
+                <Text>No data</Text>
+              </ListItem>
+            ) : (
+              this.state.junkFood.map(item => (
+                <ListItem key={item.name}>
+                  <Text>{item.name}</Text>
+                </ListItem>
+              ))
+            )}
+          </List>
         </Content>
       </Container>
     );
